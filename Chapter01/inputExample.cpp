870716cb3f0a7d23dd1a3315b27f86f4588e215a
Erik Gabrielsen <egabrielsen@smu.edu>
2020-01-27 10:14:20 -0600

    updating files

--- Chapter01/inputExample.cpp
+++ Chapter01/inputExample.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
   cout << "Input Some Information" << endl;
+  double value;
+
+  cin >> value;
+  
+  cout << "Value Read In: " << value << endl;
+
   
   return 0;
 }
